da71daaef9b9c7bb343f7a533edfaffe9895b619
BifinJ <jamesbifin@gmail.com>
2024-05-04 01:18:42 +0530

    ksmdks

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+export default function LandingPage() {
+  return (
+    <section className="home">
+      <div className="card">
+        <h3>MENTAL HEALTH THAT MEETS PEOPLE WHERE THEY ARE</h3>
+        <p>CLINICALLY VALIDATED AI GIVES IMMEDIATE SUPPORT AS  FIRST STEP OF CARE</p>
+        <button style={{padding: "10px", marginTop: "20px", fontSize: "16px", background: "lightblue", borderRadius: "10px"}}>Login/Signup</button>
+      </div>
+      <button className="next">NEXT</button>
+    </section>
+  );
+}
