1c436d32a86c7bb008ac43ce772e12107430e740
BifinJ <jamesbifin@gmail.com>
2024-05-04 09:44:06 +0530

    commit

---
+++ src/pages/ChatInterface.tsx
@@ -0,0 +1,18 @@
+import { FaMicrophoneAlt } from "react-icons/fa";
+
+export default function ChatInterface() {
+  return (
+    <section className="chatUI">
+      <div>
+        <h3>ChatBotName</h3>
+      </div>
+      <div></div>
+      <div className="chat-bottom">
+        {/* <button className="microphone">
+          <FaMicrophoneAlt />
+        </button> */}
+        <video src="public/audio.gif"></video>
+      </div>
+    </section>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom';
+
 export default function LandingPage() {
   return (
     <section className="home">
-      <div className="card">
-        <h3>MENTAL HEALTH THAT MEETS PEOPLE WHERE THEY ARE</h3>
-        <p>CLINICALLY VALIDATED AI GIVES IMMEDIATE SUPPORT AS  FIRST STEP OF CARE</p>
-        <button style={{padding: "10px", marginTop: "20px", fontSize: "16px", background: "lightblue", borderRadius: "10px"}}>Login/Signup</button>
+      <div className="card-container">
+        <div className="card">
+          <h3>MENTAL HEALTH THAT MEETS PEOPLE WHERE THEY ARE</h3>
+          <p>CLINICALLY VALIDATED AI GIVES IMMEDIATE SUPPORT AS  FIRST STEP OF CARE</p>
+          <Link to="/signup"><button style={{padding: "10px", marginTop: "20px", fontSize: "16px", background: "lightblue", borderRadius: "10px"}}>Login/Signup</button></Link>
+        </div>
       </div>
-      <button className="next">NEXT</button>
+      <Link to="/chat" style={{display: "flex", justifyContent: "center"}}><button className="next">NEXT</button></Link>
     </section>
   );
 }
